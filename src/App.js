065f890ca3856a37ac90a858c60b1d4d3602c158
Prathamesh Jadhav <prathameshjc@gmail.com>
2021-03-19 17:27:46 +0530

    parallex issue solved and background image displayed successfully

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Mycarousal from './components/mycarousal/Mycarousal';
 import { Mynavbar } from "./components/mynavbar/Mynavbar";
 import TitleMessage from './components/titlemessage/TitleMessage';
+import About from './pages/about/About';
+import Fade from 'react-reveal/Fade';
+import { Parallax } from 'react-parallax';
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
@@ -10,6 +14,22 @@ function App() {
       <Mynavbar />
       <Mycarousal />
       <TitleMessage />
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
     </div>
   );
 }
